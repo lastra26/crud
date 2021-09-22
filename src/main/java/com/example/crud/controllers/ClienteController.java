960d29917627c963c4f33ca7a92080785e116dfa
SERVIDOR\Servidor <lastra261989@gmail.com>
2021-09-22 12:16:30 -0500

    Se Agregaron correcciones en fecha para el formulario del cliente

--- src/main/java/com/example/crud/controllers/ClienteController.java
+++ src/main/java/com/example/crud/controllers/ClienteController.java
@@ -22,21 +22,22 @@ import java.util.Date;
 import java.util.Map;
 
 @Controller
-@SessionAttributes("cliente") //es mejor usar un Session attribute es mucho mas seguro podemos guardar el objeto del formulario
+@SessionAttributes("cliente")
+//es mejor usar un Session attribute es mucho mas seguro podemos guardar el objeto del formulario
 public class ClienteController {
 
     @Autowired
     private IClienteService clienteService;
 
-    @RequestMapping(value="/listar", method = RequestMethod.GET)
-    public String listar(Model model){
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String listar(Model model) {
         model.addAttribute("titulo", "Listado de Clientes");
         model.addAttribute("clientes", clienteService.findAll());
         return "listar";
     }
 
     @RequestMapping(value = "/form")
-    public String crear(Map<String, Object> model){
+    public String crear(Map<String, Object> model) {
         Cliente cliente = new Cliente();
         model.put("cliente", cliente);
         model.put("titulo", "Formulario de cliente");
@@ -45,72 +46,67 @@ public class ClienteController {
     }
 
     @RequestMapping(value = "/index")
-    public String index(Model model){
+    public String index(Model model) {
         return "/index";
     }
 
-    @RequestMapping(value="/form/{id}")
-    public String editar(@PathVariable(value ="id") Long id, Map<String, Object> model, RedirectAttributes flash){
-            Cliente cliente = null;
+    @RequestMapping(value = "/form/{id}")
+    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+        Cliente cliente = null;
 
-            if(id > 0){
-                cliente = clienteService.finOne(id);
-                if (cliente == null){
-                    flash.addFlashAttribute("error", "El id no existe en la base de datos");
-                    return "redirect:/listar";
-                }
-            }else{
-                flash.addFlashAttribute("error", "El Id del cliente no puede ser 0");
+        if (id > 0) {
+            cliente = clienteService.finOne(id);
+            if (cliente == null) {
+                flash.addFlashAttribute("error", "El id no existe en la base de datos");
                 return "redirect:/listar";
             }
-            model.put("cliente", cliente);
-            model.put("titulo", "editar cliente");
-            return "form";
+        } else {
+            flash.addFlashAttribute("error", "El Id del cliente no puede ser 0");
+            return "redirect:/listar";
+        }
+        model.put("cliente", cliente);
+        model.put("titulo", "editar cliente");
+        return "form";
     }
 
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-        public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus sessionStatus){ //aqui guardamos el objeto del dao
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus sessionStatus) { //aqui guardamos el objeto del dao
         //para el @Valid siempre va de la mano el objeto que esta mapeado y despues el binding result
-        if(result.hasErrors()){
-            model.addAttribute("titulo","formulario de cliente");
-            return"form";
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "formulario de cliente");
+            return "form";
         }
         Date fechaActual = new Date();
         System.out.println("fechaActual = " + fechaActual);
         Date fechaFormulario = cliente.getCreateAt();
         System.out.println("fecha Formulario = " + fechaFormulario);
 
-        if(fechaFormulario.before(fechaActual)){
+        if (fechaFormulario.before(fechaActual)) {
             System.out.println("si funciona");
-            String mensajeFlash = (cliente.getId() != null)? "Cliente editado con éxito" : "Cliente creado con éxito";
+            String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito" : "Cliente creado con éxito";
             clienteService.save(cliente); //recibe los datos mapeados de cliente
             sessionStatus.setComplete();
             flash.addFlashAttribute("success", mensajeFlash);
             return "redirect:listar";
-        }else if (fechaFormulario.after(fechaActual)){
-            if(result.hasErrors()){
-                System.out.println("no funciona");
-                model.addAttribute("titulo","formulario de cliente");
-                return"form";
-            }
+        } else if (fechaFormulario.after(fechaActual)) {
             System.out.println("no funciona");
-            model.addAttribute("titulo","formulario de cliente");
-            String mensajeFlash = (fechaFormulario != fechaActual)? "Fecha debe ser Menor a la Actual"
+            model.addAttribute("titulo", "formulario de cliente");
+            String mensajeFlash = (fechaFormulario != fechaActual) ? "Fecha debe ser Menor a la Actual"
                     : "Fecha debe ser Menor a la Actual";
             flash.addFlashAttribute("error", mensajeFlash);
-            return"redirect:form";
+            return "redirect:form";
         }
         return "redirect:listar";
     }
 
-    @RequestMapping(value="/eliminar/{id}")
+    @RequestMapping(value = "/eliminar/{id}")
     public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
-    if (id > 0 ){
-        clienteService.delete(id);
-        flash.addFlashAttribute("success", "Eliminado Con éxito");
-    }
-    return"redirect:/listar";
+        if (id > 0) {
+            clienteService.delete(id);
+            flash.addFlashAttribute("success", "Eliminado Con éxito");
+        }
+        return "redirect:/listar";
     }
 }
 
